6284a667e1e1bb196d57e212370510bf2ff89a20
mandip chaudhary <mandip.chaudhary2022@pcmgmt.edu.np>
2025-09-05 15:54:52 +0545

    aaded chnage in vite file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import vitePluginSitemap from 'vite-plugin-sitemap'  // ✅ default import
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    vitePluginSitemap({
+      hostname: 'https://samrat-movies.vercel.app', // your deployed domain
+      dynamicRoutes: [
+        '/',             
+        '/portfolio',
+        '/online',
+        '/services/tranning',
+        '/services/wedding',
+        '/services/studio',
+        '/rental/camera',
+        '/rental/projector',
+        '/rental/drone',
+        '/rental/studio',
+        '/service/web',
+        '/service/webdev',
+        '/service/social',
+        '/service/graphic',
+        '/contact',
+      ],
+    }),
   ],
 })
