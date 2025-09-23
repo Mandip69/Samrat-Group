c82949de60eabe07195489f2c32934704de23e84
mandip chaudhary <mandip.chaudhary2022@pcmgmt.edu.np>
2025-09-23 14:28:02 +0545

    Fix image extensions to lowercase for hosting

--- src/pages/portfolio/portfolioData.js
+++ src/pages/portfolio/portfolioData.js
@@ -3,7 +3,7 @@ export const categories = {
   award: { folder: "awarded", prefix: "d", count: 15 }, // your awarded images
   wedding: { folder: "images", prefix: "we", count: 30 },
   food: { folder: "images", prefix: "fod", count: 6 },
-   Pasni: { folder: "images", prefix: "pas", count: 8 },
+ Pasni: { folder: "images", prefix: "pas", count: 8 },
  Bartabanda: { folder: "images", prefix: "bar", count: 10 },
  Program: { folder: "images", prefix: "tej", count: 5 },
  Shoot: { folder: "images", prefix: "per", count: 3 },
